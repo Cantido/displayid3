13bbe75d101974f5a574f18f64f788a1e092ddf6
Robert Richter <robert.c.richter@gmail.com>
2013-05-16 15:33:44 -0500

    Apply license [Closes #13]

--- displayid3.c
+++ displayid3.c
@@ -1,14 +1,20 @@
-/******************************************************************************\
+/*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *
  * verifyid3.c - Displays information in an ID3v2 header
- * Author: Robert Richter - robert.c.richter@gmail.com
- * Date: June 2012
- * Description: Using the rules for an ID3v2 header given at
- *   http://id3.org/d3v2.3.0, this program reads and displays the information
- *   contained in a file's ID3v2 tag. Uses the same source as my verifyid3
- *   program, available at https://github.com/Cantido/verifyid3
-\******************************************************************************/
-
-#include <sys/types.h>
+ * Copyright (C) 2013 Robert Richter
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  */
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
